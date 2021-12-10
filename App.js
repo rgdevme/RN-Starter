54185104067da4b2bbe60ad5627334883f1e3668
Rafael Gonzalez <rafael.gonzalez81@gmail.com>
2021-12-09 20:05:49 -0400

    Routes

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import HomeScreen from "./src/screens/HomeScreen";
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import Home from './src/screens/Home'
+import BlogPosts from './src/screens/BlogPosts'
+import Counter from './src/screens/Counter'
+import RandomColors from './src/screens/RandomColors'
+import Test from './src/screens/Test'
+import TextInput from './src/screens/TextInput'
+import TutorialExcercises from './src/screens/TutorialExcercises'
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen,
+    Home,
+    BlogPosts,
+    Counter,
+    RandomColors,
+    Test,
+    TextInput,
+    Tutorial: TutorialExcercises
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: 'Home',
     defaultNavigationOptions: {
-      title: "App",
-    },
+      title: 'App'
+    }
   }
-);
+)
 
-export default createAppContainer(navigator);
+export default createAppContainer(navigator)
