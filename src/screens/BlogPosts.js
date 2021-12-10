17393b14a8f86119e0ea75ba929300a1e13ab953
Rafael Gonzalez <rafael.gonzalez81@gmail.com>
2021-12-09 20:06:31 -0400

    Boileplate for future component

---
+++ src/screens/BlogPosts.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, StyleSheet, Button, View, TouchableOpacity } from 'react-native'
+
+const BlogPosts = () => {
+  return <View />
+}
+
+const styles = StyleSheet.create({})
+
+export default BlogPosts
